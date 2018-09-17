9eed579e8eaaadf6b575786ab0f87770362bac3d
HanSangKwon <HanSangKwon@users.noreply.github.com>
2018-09-17 21:24:58 +0900

    로그인 화면 화면만 만듬!

---
+++ UnityClient/Assets/Script/Play/GameManager_.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yuch_;
+
+
+public class GameManager_ : MonoBehaviour {
+
+    Yuch yuch = new Yuch();
+    RerultYuch rerultYuch;
+
+    public GameObject Player1;
+
+    int sequence = 1;
+
+
+
+    private void Update()
+    {
+        switch(sequence)
+        {
+            case 1:
+                sequence = 2;
+                break;
+            case 2:
+                sequence = 3;
+                break;
+            case 3:
+                sequence = 4;
+                break;
+            case 4:
+                sequence = 5;
+                break;
+            case 5:
+                sequence = 6;
+                break;
+
+        }
+
+    }
+
+    public void P1_Throwing()
+    {
+        rerultYuch = yuch.ThrowYuch();
+        Debug.Log("Player_1:" +  rerultYuch);
+        GameObject.Find("Player_1").GetComponent<Player_>().SetActive(rerultYuch);  
+    }
+
+
+}
