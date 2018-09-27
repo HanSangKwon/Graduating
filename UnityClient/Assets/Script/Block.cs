d41deb4695d4f2ad6c6745cabf25f1bc1287c1ac
HanSangKwon <HanSangKwon@users.noreply.github.com>
2018-09-27 20:45:57 +0900

    유니티 클라 만드는 중 참고자료 https://www.youtube.com/watch?v=VzhhaIbzGNY

---
+++ UnityClient/Assets/Script/Block.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+
+    float fall = 0;
+    public float fallSpeed = 1;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UserInput();
+
+
+    }
+    void UserInput()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, 90);
+
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
+        {
+            transform.position += new Vector3(0, -1, 0);
+
+            fall = Time.time;
+
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+            transform.Rotate(0, 0, 90);
+
+
+
+    }
+
+}
